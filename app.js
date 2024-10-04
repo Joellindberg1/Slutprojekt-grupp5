3878260c0bb25142d458aab64a8ff42f55b12eb6
Harry Chichon <harry.chichon@proton.me>
2024-10-04 11:42:45 +0200

    Div. ändringar

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-MONSTER - BRANCH
+// ==================OBJEKT==================
+
+const monsterObject = {
+
+    Name: ;// Behöver hämtas från formuläret
+    Type: monsterTypes;
+    Color: monsterColors;
+    Number of tentacles: ; // Behöver hämtas från formuläret
+    Number of eyes: ; // Behöver hämtas från formuläret
+    Number of arms: ; // Behöver hämtas från formuläret
+    Number of horns: ; // Behöver hämtas från formuläret
+
+    colorMap: {
+        color1: monsterColors[0],
+        color2: monsterColors[1],
+        color3: monsterColors[2],
+        color4: monsterColors[3],
+        color5: monsterColors[4]
+    }
+}
+
+//===========================================
+
+// Ändrar färgen av monster-kortets box-shadow till en ton av monstrets färg
+// switch (monsterObject.Color) {
+
+//     case monsterColors[0]:
+
+//         break;
+
+//     case monsterColors[1]:
+//         break;
+
+//     case monsterColors[2]:
+//         break;
+
+//     case monsterColors[3]:
+//         break;
+
+//     case monsterColors[4]:
+//         break;
+// }
+
