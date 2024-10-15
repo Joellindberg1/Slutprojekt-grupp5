87fd313b9a9389000fbe598e59493f9a2cf9f39e
sania <sania_19_88_@hotmail.com>
2024-10-15 10:12:51 +0200

    css-form

--- app.js
+++ app.js
@@ -63,96 +63,90 @@
 // ===========================================
 
 const renderMonsterCard = () => {
+  // Tömmer section class="collection"
+  const monsterArticle = document.querySelector(".collection");
+  monsterArticle.innerHTML = "";
 
-    // Tömmer section class="collection"
-    const monsterArticle = document.querySelector(".collection");
-    monsterArticle.innerHTML = "";
+  // loopar igenom state för att skapa monster-kort av innehållet
+  for (const m of state.collection) {
+    // Skapar ny article-tag
+    const monster = document.createElement("article");
 
-    // loopar igenom state för att skapa monster-kort av innehållet
-    for (const m of state.collection) {
+    // Skapar tabell och caption till den nyskapade article-tag'en
+    const table = document.createElement("table");
+    const caption = document.createElement("caption");
+    const h3 = document.createElement("h3");
+    h3.textContent = m.name;
+    caption.appendChild(h3);
+    table.appendChild(caption);
 
-        // Skapar ny article-tag
-        const monster = document.createElement("article");
+    // Skapar rader till tabellen
+    const addRow = (label, value) => {
+      const row = document.createElement("tr");
+      const th = document.createElement("th");
+      th.scope = "row";
+      th.textContent = `${label}: `;
+      const td = document.createElement("td");
+      td.textContent = value;
+      row.appendChild(th);
+      row.appendChild(td);
+      table.appendChild(row);
+    };
 
-        // Skapar tabell och caption till den nyskapade article-tag'en
-        const table = document.createElement("table");
-        const caption = document.createElement("caption");
-        const h3 = document.createElement("h3");
-        h3.textContent = m.name;
-        caption.appendChild(h3);
-        table.appendChild(caption);
+    // Lägger till rader till den nyskapade tabellen
+    addRow("Type", m.type);
+    addRow("Color", m.color);
+    addRow("Number of eyes", m.eyes);
+    addRow("Number of arms", m.arms);
+    addRow("Number of horns", m.horns);
+    addRow("Number of tentacles", m.tentacles);
 
-        // Skapar rader till tabellen
-        const addRow = (label, value) => {
-            const row = document.createElement("tr");
-            const th = document.createElement("th");
-            th.scope = "row";
-            th.textContent = `${label}: `;
-            const td = document.createElement("td");
-            td.textContent = value;
-            row.appendChild(th);
-            row.appendChild(td);
-            table.appendChild(row);
-        }
+    // Lägger till tabellen i monster-containern
+    monster.appendChild(table);
 
-        // Lägger till rader till den nyskapade tabellen
-        addRow("Type", m.type);
-        addRow("Color", m.color);
-        addRow("Number of eyes", m.eyes);
-        addRow("Number of arms", m.arms);
-        addRow("Number of horns", m.horns);
-        addRow("Number of tentacles", m.tentacles);
+    // Skapar en knapp för att redigera monster-attributen
+    const editButton = document.createElement("button");
+    editButton.className = "edit-button";
+    editButton.type = "button";
+    editButton.innerHTML = "Edit monster";
 
-        // Lägger till tabellen i monster-containern
-        monster.appendChild(table);
+    // Ger monster-containern en class (så att önskad CSS kan appliceras)
+    monster.className = "monster";
 
-        // Skapar en knapp för att redigera monster-attributen
-        const editButton = document.createElement("button");
-        editButton.className = "edit-button";
-        editButton.type = "button";
-        editButton.innerHTML = "Edit monster";
+    // Lägger till redigeringsknappen i monster-containern
+    monster.appendChild(editButton);
 
-        // Ger monster-containern en class (så att önskad CSS kan appliceras)
-        monster.className = "monster";
-
-        // Lägger till redigeringsknappen i monster-containern
-        monster.appendChild(editButton);
-
-
-        //Lägger till monster-containern i DOM
-        monsterArticle.appendChild(monster);
-
-    }
+    //Lägger till monster-containern i DOM
+    monsterArticle.appendChild(monster);
+  }
 };
-
 
 // ========LÄGGER IN ARRAYS I FORMULÄR========
 // Funktion som skapar dynamiska monsterkort.
 // ===========================================
 window.addEventListener("load", (event) => {
-    i = 0;
-    for (const element of monsterColors) {
-        const colors = document.getElementById("monster-Colors");
-        colors.value = i;
-        colors.innerHtml = monsterColors[i]
-        i++;
-        const option = document.createElement("option");
-        option.text = element;
-        colors.add(option);
-    }
+  i = 0;
+  for (const element of monsterColors) {
+    const colors = document.getElementById("monster-Colors");
+    colors.value = i;
+    colors.innerHtml = monsterColors[i];
+    i++;
+    const option = document.createElement("option");
+    option.text = element;
+    colors.add(option);
+  }
 
-    i = 0;
-    for (const element of monsterTypes) {
-        const types = document.getElementById("monster-Types");
-        types.value = i;
-        types.innerHtml = monsterTypes[i]
-        i++;
-        const option = document.createElement("option");
-        option.text = element;
-        types.add(option);
-    }
+  i = 0;
+  for (const element of monsterTypes) {
+    const types = document.getElementById("monster-Types");
+    types.value = i;
+    types.innerHtml = monsterTypes[i];
+    i++;
+    const option = document.createElement("option");
+    option.text = element;
+    types.add(option);
+  }
 });
-
 
 // ==============APP STARTAR HÄR==============
 
@@ -162,30 +156,95 @@ const monsterTypes = ["Firemonster", "Skymonster", "Watermonster"];
 
 // State innehåller datan från användarens input samt standardvärdena för appen
 const state = {
-
-    collection: [
-        { name: "Grimblot", type: monsterTypes[1], color: monsterColors[1], eyes: 1, arms: 2, horns: 2, tentacles: 0 },
-        { name: "Zarok the Devourer", type: monsterTypes[0], color: monsterColors[0], eyes: 2, arms: 0, horns: 1, tentacles: 4 },
-        { name: "Blisterfang", type: monsterTypes[1], color: monsterColors[1], eyes: 3, arms: 4, horns: 0, tentacles: 1 },
-        { name: "Thraxxis", type: monsterTypes[2], color: monsterColors[2], eyes: 5, arms: 8, horns: 1, tentacles: 0 },
-        { name: "Murkspawn", type: monsterTypes[0], color: monsterColors[3], eyes: 2, arms: 2, horns: 0, tentacles: 6 },
-        { name: "Vorrgath", type: monsterTypes[1], color: monsterColors[4], eyes: 1, arms: 1, horns: 1, tentacles: 1 },
-    ],
-    createNewMonster: [],
-    addMonster: (name, type, color, eyes, arms, horns, tentacles) => {
-        state.collection.push({ name, type, color, eyes, arms, horns, tentacles });
+  collection: [
+    {
+      name: "Grimblot",
+      type: monsterTypes[1],
+      color: monsterColors[1],
+      eyes: 1,
+      arms: 2,
+      horns: 2,
+      tentacles: 0,
     },
+    {
+      name: "Zarok the Devourer",
+      type: monsterTypes[0],
+      color: monsterColors[0],
+      eyes: 2,
+      arms: 0,
+      horns: 1,
+      tentacles: 4,
+    },
+    {
+      name: "Blisterfang",
+      type: monsterTypes[1],
+      color: monsterColors[1],
+      eyes: 3,
+      arms: 4,
+      horns: 0,
+      tentacles: 1,
+    },
+    {
+      name: "Thraxxis",
+      type: monsterTypes[2],
+      color: monsterColors[2],
+      eyes: 5,
+      arms: 8,
+      horns: 1,
+      tentacles: 0,
+    },
+    {
+      name: "Murkspawn",
+      type: monsterTypes[0],
+      color: monsterColors[3],
+      eyes: 2,
+      arms: 2,
+      horns: 0,
+      tentacles: 6,
+    },
+    {
+      name: "Vorrgath",
+      type: monsterTypes[1],
+      color: monsterColors[4],
+      eyes: 1,
+      arms: 1,
+      horns: 1,
+      tentacles: 1,
+    },
+  ],
+  createNewMonster: [],
+  addMonster: (name, type, color, eyes, arms, horns, tentacles) => {
+    state.collection.push({ name, type, color, eyes, arms, horns, tentacles });
+    state.addMonster(
+      name,
+      type,
+      color,
+      parseInt(eyes),
+      parseInt(arms),
+      parseInt(horns),
+      parseInt(tentacles)
+    );
+  },
 };
 
 state.addMonster("Test", monsterTypes[1], monsterColors[2], 2, 3, 4, 5);
 renderMonsterCard();
 
+const button = document.getElementById("form-button");
+button.addEventListener("click", function () {
+  const name = document.getElementById("name").value;
+  const color = document.getElementById("monster-Colors").value;
+  const type = document.getElementById("monster-Types").value;
+  const eyes = document.querySelector('input[name="eyes"]').value;
+  const arms = document.querySelector('input[name="arms"]').value;
+  const horns = document.querySelector('input[name="horn"]').value;
+  const tentacles = document.querySelector('input[name="tentacles"]').value;
 
+  document.getElementById("output").textContent = "Done!";
+});
 document.querySelector(".edit-button").addEventListener("click", (e) => {
-    e.preventDefault();
-    // Anropa funktion som ersätter monsterkortets innehåll med ett formulär för att redigera monstrets attribut. Nuvarande data ska behållas i inputrutorna. (OBS! Jag har inte börjat bygga funktionen ännu)
-
+  e.preventDefault();
+  // Anropa funktion som ersätter monsterkortets innehåll med ett formulär för att redigera monstrets attribut. Nuvarande data ska behållas i inputrutorna. (OBS! Jag har inte börjat bygga funktionen ännu)
 });
 
 // Lägg till funktion här som tar information från formuläret och lägger in som ett monster-objekt i state.collection
-
